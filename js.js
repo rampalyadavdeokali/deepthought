e95d440ea9923c8ec57ef2bb3a9dc692083ef645
Piyush <guptapiyush4444@gmail.com>
2024-07-25 13:56:42 +0530

    Done

--- js.js
+++ js.js
@@ -3,31 +3,93 @@ const leftPanel = document.querySelector('.leftPanel');
 const ul = document.querySelector('#ul');
 const one = document.querySelector('.one');
 
+// site initial effect 
+
+function gsapEffect() {
+    gsap.from(leftPanel, {
+        delay: 0.5,
+        x: -110,
+        duration: 0.5,
+
+    });
+
+    gsap.from(".rightPanel", {
+        delay: 0.5,
+        x: 100,
+        duration: 0.5,
+
+    });
+
+    gsap.from(".Explore , .Technical, .card", {
+        delay: 0.2,
+        scale: .9,
+        duration: 0.5,
+        opacity: 0,
+        zIndex: 5,
+    });
+
+    gsap.from(".navItems li", {
+        delay: 0.2,
+        y: -50,
+        duration: .8,
+        opacity: 0,
+        stagger: 0.12,
+        ease: "elastic.out(.4,0.5)"
+    });
+}
+gsapEffect();
+
+
 // Journey Board Opne & Close Logic 
+const tl = gsap.timeline({ paused: true });
 leftPanelBtn.addEventListener('click', () => {
 
     if (leftPanelBtn.className == "ri-arrow-right-circle-fill") {
         ul.style.opacity = 1;
         one.style.opacity = 0;
-        leftPanel.style.left = 0;
         leftPanelBtn.classList.replace("ri-arrow-right-circle-fill", "ri-arrow-left-circle-fill");
+
+        // GSAP code for smooth transition 
+        tl.to(leftPanel, {
+            left: 0,
+            duration: 0.3
+        });
+
+        tl.from("#ulbody li", {
+            x: -100,
+            duration: 0.2,
+            stagger: 0.12,
+            opacity: 0
+        });
+
 
     }
     else {
-        ul.style.opacity = 0;
+
         one.style.opacity = 1;
-        leftPanel.style.left = "-" + 285 + "px";
         leftPanelBtn.classList.replace("ri-arrow-left-circle-fill", "ri-arrow-right-circle-fill");
 
+        // GSAP code for smooth transition 
+        tl.to(ul, {
+            x: -6,
+            opacity: 0,
+            duration: 0.3,
+        });
+
+        tl.to(leftPanel, {
+            left: -285,
+            duration: 0.3,
+        });
     }
+});
 
-
+leftPanelBtn.addEventListener("click", () => {
+    tl.play();
 });
 
 
 // JSON Data update logic 
 const url = 'ddugky_project.json';
-
 function fetchJSONData() {
     cardId = [18882, 18883, 18884, 18885, 18886];
 
